8c5cc973c6227e0aaa4a975f2d86e604a736261b
Isfar Oshir <iao233@nyu.edu>
2021-04-02 16:14:01 -0400

    first day

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,65 @@ const bodyParser = require('body-parser')   //import
 
 const app = express()               // creating an instance of express
 
-app.use(bodyParser.json())          //allows to send json //allows.json method
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  
+    next();
+  });
 
-let database = {
-    'delta': {
-        'airplanes': 4,
-        'location': 'NYC'
-    },
-    'united': {
-        'airplanes': 2,
-        'location': 'New Jersey'
-    }
-}
+app.use(bodyParser.json())          //allows to send json //allows.json method
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'bht5nlo6upctnbmxntso-mysql.services.clever-cloud.com',
+  user     : 'uzvcitr36qb3fwiw',
+  password : 'gnm9ZRsUBFPCoy95Edkn',
+  database : 'bht5nlo6upctnbmxntso'
+});
+
+connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);  //results is an array of objects
+// });
+
+// connection.query('SELECT * FROM Customer', function(error, results, fields) {
+//     if (error) throw error;
+//     console.log(results);
+// })
+
+app.post('/api/customer/register', function(req, res){
+    console.log(req.body.email);
+    
+    var arr = [req.body.email,req.body.name,req.body.password,req.body.building_number,req.body.street,req.body.city,req.body.state,
+        req.body.phone_number,req.body.passport_number,req.body.passport_expiration,req.body.passport_country,req.body.date_of_birth]
+    let dynamic = ''
+    arr.forEach((element, index) => {
+        if (index == arr.length-1){
+            dynamic += element
+        } else{
+            dynamic += element + ','
+        }
+});
+
+    const begin = 'INSERT INTO `Customer` (`email`,`name`,`password`,`building_number`,`street`,`city`,`state`,`phone_number`,`passport_number`,`passport_expiration`,`passport_country`,`date_of_birth`)'
+    
+    connection.query(begin + ' VALUES (' + dynamic + ')', function (err, results, fields){
+        if (err) throw err;
+        res.json({'status': 'registered'})
+    })
+    res.json({'status': 'invalid'})
+})
+
+
 
 app.get('/api/:airLineID', function(req, res){      // the colon makes it so its flexible
     console.log(req.params.airLineID)               //this prints the users parameter
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,41 @@
 import React, {useState} from 'react'
 import NavBar from '../shared/NavBar'
+import axios from 'axios'
 import {InputGroup, FormControl, Modal, Button} from 'react-bootstrap'
 
 function HomePage(){
     const [show, setShow] = useState(false);
     const [show2, setShow2] = useState(false)
+
+    const [registerEvent, setRegisterEvent] = useState({
+        email: "", name: "", password: "", building_number: "",
+        street: "", city: "", state: "", passport_number: "", phone_number: "",
+        passport_expiration: "", passport_country: "", date_of_birth: ""
+    })
+    
+
+    const registerChangeHandler = (event) => {
+        setRegisterEvent({
+            ...registerEvent,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const registerButtonHandler = () => {
+        console.log(registerEvent)
+        axios.post('http://localhost:8000/api/customer/register', registerEvent).then( response => {
+            if (response.data.status === "registered"){
+                window.location = '/'
+            }
+            else{
+                alert('Username or Email already exists')
+            }
+        }
+    )
+        // window.location = '/'
+    }
+
+    
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -18,7 +49,7 @@ function HomePage(){
                 nav={[['/', 'Features'], ['#pricing', 'Flight Tracker']]} 
                 accountManagement={[handleShow, handleShow2]}
             />
-            <Modal show={show} onHide={handleClose}>
+            <Modal centered show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                 <Modal.Title>Sign In</Modal.Title>
                 </Modal.Header>
@@ -39,7 +70,67 @@ function HomePage(){
                 <Modal.Title>Sign Up</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    Woohoo, you're reading this text in a modal!
+                    <form>
+                        <label>Name</label>
+                        <br/>
+                        <input name="name" placeholder="Name" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Address Line 1: Building Number</label>
+                        <br/>
+                        <input name="building_number" placeholder="Building Number" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Address Line 2: Street</label>
+                        <br/>
+                        <input name="street" placeholder="street" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>City</label>
+                        <br/>
+                        <input name="city" placeholder="city" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>State</label>
+                        <br/>
+                        <input name="state" placeholder="state" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Phone Number</label>
+                        <br/>
+                        <input type="tel" name="phone_number" placeholder="Phone Number" onChange={registerChangeHandler} required/>
+                        <br />
+                        <br />
+                        <label>Passport #</label>
+                        <br/>
+                        <input name="passport_number" placeholder="Passport #" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Passport Expiration</label>
+                        <br/>
+                        <input type="date" name="passport_expiration" placeholder="Passport Expiration" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Passport Country</label>
+                        <br/>
+                        <input name="passport_country" placeholder="Country of passport" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Date of Birth</label>
+                        <br/>
+                        <input type="date" name="date_of_birth" placeholder="Date of Birth" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Email</label>
+                        <br/>
+                        <input type="email" name="email" placeholder="Email" onChange={registerChangeHandler} required/>
+                        <br/>
+                        <br/>
+                        <label>Password</label>
+                        <br/>
+                        <input type="password" name="password" placeholder="Password" onChange={registerChangeHandler} required/>
+                        <br /><br />
+                        <button type="button" className="lg" onClick={registerButtonHandler}>Register</button>                    </form>
                 </Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose2}>
