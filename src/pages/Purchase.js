c7f83d63b853665b6e61c472f6856d7ae286414b
Farhan Mashud <fm1539@nyu.edu>
2021-04-18 15:32:47 -0400

    handling payments

--- src/pages/Purchase.js
+++ src/pages/Purchase.js
@@ -1,5 +1,5 @@
 import {useParams} from 'react-router-dom'
-import React from 'react';
+import {React, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {Button} from 'react-bootstrap'
 import {loadStripe} from '@stripe/stripe-js';
@@ -11,14 +11,34 @@ import {
 } from '@stripe/react-stripe-js';
 import axios from 'axios'
 
+const SubmitButton = ({ processing, error, children, disabled }) => (
+  <button
+    className={`SubmitButton ${error ? "SubmitButton--error" : ""}`}
+    type="submit"
+    disabled={processing || disabled}
+  >
+    {processing ? "Processing..." : children}
+  </button>
+);
+
+const CardField = ({ onChange }) => (
+  <div className="FormRow">
+    <CardElement onChange={onChange} />
+  </div>
+);
+
 const CheckoutForm = () => {
   let {airlineName, flightNumber, departDate, departTime, basePrice} = useParams()
-  console.log(flightNumber, departDate, departTime, basePrice);
   const stripe = useStripe();
   const elements = useElements();
+  const [error, setError] = useState(null)
+  const [cardComplete, setCardComplete] = useState(false)
+  const [processing, setProcessing] = useState(false)
+  const [paymentMethod, setPaymentMethod] = useState(null)
 
+  
+    
   const handleSubmit = async (event) => {
-    console.log(event);
     // Block native form submission.
     event.preventDefault();
 
@@ -33,17 +53,32 @@ const CheckoutForm = () => {
     // each type of element.
     const cardElement = elements.getElement(CardElement);
 
+    if (error) {
+      cardElement.focus();
+      return;
+    }
+
+    if (cardComplete) {
+      console.log("Hi");
+      setProcessing(true)
+    }
+
     // Use your card Element with other Stripe.js APIs
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
+    const payload = await stripe.createPaymentMethod({
       type: 'card',
       card: cardElement,
     });
 
-    if (error) {
-      console.log('[error]', error);
-    } else {
-      console.log('[PaymentMethod]', paymentMethod);
-    }
+
+    // if (error) {
+    //   console.log('[error]', error);
+    // } else {
+    //   console.log('[PaymentMethod]', paymentMethod);
+    // }
+
+    if (payload.error) setError(payload.error)
+    else setPaymentMethod(payload.paymentMethod)
+
     let obj = {
       email: JSON.parse(localStorage.getItem('custObj')).email,
       airline_name: airlineName,
@@ -52,11 +87,16 @@ const CheckoutForm = () => {
       depart_time: departTime,
       base_price: basePrice
     }
-    axios.post('http://localhost:8000/api/customer/purchaseTickets', obj).then(response=>{
-      console.log(response.data);
-    })
-    window.location = '/viewFlights'
 
+    if (paymentMethod) {
+      axios.post('http://localhost:8000/api/customer/purchaseTickets', obj).then(response=>{
+        console.log(response);
+      })
+      window.location = '/viewFlights'
+    }
+
+    else alert("Payment not successful, please try again")
+  
   };
 
   return (
@@ -71,8 +111,13 @@ const CheckoutForm = () => {
               <h3>Stubborn Attachments</h3>
               <h5>${basePrice}</h5>
               <form onSubmit={handleSubmit}>
-                <CardElement />
-                <button type="submit" id="checkout-button" disabled={!stripe}>Checkout</button>
+                <CardField
+                  onChange={(e) => {
+                    setError(e.error);
+                    setCardComplete(e.complete);
+                  }}
+                />
+                <SubmitButton processing={processing} error={error} disabled={!stripe}>Checkout</SubmitButton>
               </form>
             </div>
           </div>
